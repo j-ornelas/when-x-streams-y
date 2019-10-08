7d08646c364978bc7b9924bbc82babb5efce74be
john ornelas <johnnyoh1988@gmail.com>
2019-10-08 02:06:42 -0400

    feat: handle creating subscriptions

--- server/src/database/controllers/userController.ts
+++ server/src/database/controllers/userController.ts
@@ -1,11 +1,15 @@
 import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
 import { User } from '../models/UserModel';
+import { Subscription } from '../models/SubscriptionModel';
 const router = Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true
 }));
+
+const { TWITCH_CLIENT_ID } = process.env;
 
 router.get('/get', async (req:Request, res:Response) => {
   try {
@@ -19,12 +23,58 @@ router.get('/get', async (req:Request, res:Response) => {
 interface SubRequestInterface extends Request {
   body: {
     userId: String;
-    subscriptionId: String;
+    streamName: String;
+    gameName: String;
   };
 }
 router.post('/add-subscription', async (req:SubRequestInterface, res:Response) => {
   try {
-    const { userId, subscriptionId } = req.body;
+    let { userId, streamName, gameName } = req.body;
+    // getStreamerId
+    const twitchStreamerRequest = await fetch(`https://api.twitch.tv/helix/users?login=${streamName}`, {
+      headers: {
+        "Client-ID": TWITCH_CLIENT_ID || ''
+      }
+    })
+    const streamerData = await twitchStreamerRequest.json();
+    if (!streamerData || !streamerData.data || !streamerData.data.length) return res.send({ message: `We couldn't find a streamer with the name ${streamName}` });
+    const streamerId = streamerData.data[0].id;
+    // getGameId
+    const twitchGameRequest = await fetch(`https://api.twitch.tv/helix/games?name=${gameName}`, {
+      headers: {
+        "Client-ID": TWITCH_CLIENT_ID || ''
+      }
+    })
+    const gameData = await twitchGameRequest.json();
+    if (!gameData || !gameData.data || !gameData.data.length) return res.send({ message: `We couldn't find a game with the name ${gameName}` });
+    const gameId = gameData.data[0].id;
+
+    let previousSubscription = await Subscription.findOne({
+      streamerId,
+      gameId,
+    });
+    let subscriptionId;
+
+    if (!previousSubscription) {
+      // create subscription;
+      console.log('CREATING NEW SUB')
+      await new Subscription({
+        streamerId,
+        gameId,
+        streamName,
+        gameName
+      }).save();
+
+      const newSubscription = await Subscription.findOne({
+        streamerId,
+        gameId,
+      });
+      subscriptionId = (newSubscription && newSubscription._id);
+    } else {
+      console.log('FOUND SUB IN DB');
+      subscriptionId = previousSubscription._id
+    }
+    if (!subscriptionId) return res.send({ message: 'something went wrong' });
     const user = await User.updateOne(
       { _id: userId },
       { $push: { subscriptions: subscriptionId }}
