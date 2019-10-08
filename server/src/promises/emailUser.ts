5dd2c9a47bf0eb81baf50eece8309474078fb6e9
john ornelas <johnnyoh1988@gmail.com>
2019-10-08 02:05:22 -0400

    feat: pass subscription info into notify promise

--- server/src/promises/emailUser.ts
+++ server/src/promises/emailUser.ts
@@ -1,10 +1,11 @@
 import { UserInterface } from '../database/models/UserModel';
+import { SubscriptionInterface } from '../database/models/SubscriptionModel';
+
 import nodemailer from 'nodemailer';
 
 const { EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
 
-const emailUser = (user:UserInterface) => new Promise(async (resolve, reject) => {
-  console.log('email stuff', EMAIL_ADDRESS, EMAIL_PASSWORD)
+const emailUser = (user:UserInterface, { streamName, gameName }:SubscriptionInterface) => new Promise(async (resolve, reject) => {
   const transporter = nodemailer.createTransport({
    service: 'gmail',
    auth: {
@@ -17,8 +18,13 @@ const emailUser = (user:UserInterface) => new Promise(async (resolve, reject) =>
     from: 'WXSY - no-reply', // sender address
     // to: 'bar@example.com, baz@example.com', // list of receivers
     to: user.email.toString(),
-    subject: 'A subscriber you follow just went live', // Subject line
-    html: '<h1>Hello world?</h1>' // html body
+    subject: `${streamName} is now playing ${gameName}!`, // Subject line
+    html: `
+      <h3>Greetings from WXSY!</h3>
+      <h4>${streamName.toUpperCase()} is playing ${gameName.toUpperCase()}
+      <h6>You're recieving this notification because you subscribed to this specific streamer/game combo</h6>
+      <a href=https://www.twitch.tv/${streamName}>Click here to begin watching!<a>
+    `
   };
 
   // send mail with defined transpor
--- server/src/promises/notifyUsers.ts
+++ server/src/promises/notifyUsers.ts
@@ -1,9 +1,10 @@
 import { UserInterface } from '../database/models/UserModel';
+import { SubscriptionInterface } from '../database/models/SubscriptionModel';
 import emailUser from './emailUser';
 // TODO: remove the any from this.
-const notifyUsers = (users:any) => new Promise((resolve, reject) => {
+const notifyUsers = (users:any, subscription:SubscriptionInterface) => new Promise((resolve, reject) => {
   users.forEach(async (user:UserInterface) => {
-    await emailUser(user);
+    await emailUser(user, subscription);
   });
   resolve(true);
 });
