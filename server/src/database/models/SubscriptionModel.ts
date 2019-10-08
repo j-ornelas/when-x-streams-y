745e416a27967103003c40045072860790575b7f
john ornelas <johnnyoh1988@gmail.com>
2019-10-08 02:06:18 -0400

    feat: include stream name and game name in sub model

--- server/src/database/models/SubscriptionModel.ts
+++ server/src/database/models/SubscriptionModel.ts
@@ -1,8 +1,17 @@
 import { model, Schema, Model, Document } from 'mongoose';
 
+export interface SubscriptionInterface {
+  streamerId: String;
+  gameId: String;
+  streamName: String;
+  gameName: String;
+};
+
 const SubscriptionSchema = new Schema({
   streamerId: String,
   gameId: String,
+  streamName: String,
+  gameName: String,
 });
 
 export const Subscription:Model<Document> = model('Subscription', SubscriptionSchema);
