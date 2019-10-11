ca08066ed768d22943f0a5089348a01a5a1850f5
john ornelas <johnnyoh1988@gmail.com>
2019-10-11 01:16:51 -0400

    feat: remove server/build

--- server/build/database/models.ts/SubscriptionModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var SubscriptionSchema = new mongoose_1.Schema({
-    streamerId: String,
-    gameId: String,
-});
-exports.SubscriptionModel = mongoose_1.model('Subscription', SubscriptionSchema);
