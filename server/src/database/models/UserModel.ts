447535ebbcf811c0e0c20ed23e548192191e9832
john ornelas <johnnyoh1988@gmail.com>
2019-10-13 02:59:37 -0400

    feat: add findOrCreate User static

--- server/src/database/models/UserModel.ts
+++ server/src/database/models/UserModel.ts
@@ -1,17 +1,37 @@
 import { model, Schema, Model, Document } from 'mongoose';
 
-export interface UserInterface {
-  subscriptions:[String];
-  email: String;
-  password: String;
-  phone: String;
+export interface UserInterface extends Document {
+  subscriptions:[string];
+  email: string;
+  password: string;
+  phone: string;
 };
+export interface UserModelInterface extends Model<UserInterface> {
+  findOrCreate(email:string):Promise<UserInterface>;
+}
 
 const UserSchema = new Schema({
   subscriptions: [String],
   email: String,
   password: String,
   phone: String,
-});
+})
 
-export const User:Model<Document> = model('User', UserSchema);
+UserSchema.statics.findOrCreate = function(email:string) {
+  const userModel:UserModelInterface = this;
+  return new Promise(async(resolve, reject) => {
+    try {
+      const prevUser = await userModel.findOne({ email });
+      if (prevUser) {
+        return resolve(prevUser);
+      } else {
+        resolve(await userModel.create({ email }))
+      }
+    } catch (err) {
+      reject(err);
+    }
+  })
+}
+
+
+export const User:UserModelInterface = model<UserInterface, UserModelInterface>('User', UserSchema);
